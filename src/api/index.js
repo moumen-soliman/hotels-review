bc5e4e8a11304ea5b445e0bb692300a1ae958664
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 01:01:38 +0200

    Fix format

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default () => axios.create({
-  baseURL: 'http://my-json-server.typicode.com/fly365com/code-challenge',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'X-Requested-With',
-    'Access-Control-Allow-Methods': 'GET'
-  }
-});
+export default () =>
+  axios.create({
+    baseURL: "http://my-json-server.typicode.com/fly365com/code-challenge",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-Requested-With": "X-Requested-With",
+      "Access-Control-Allow-Methods": "GET"
+    }
+  });
