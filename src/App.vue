3b86dd1444fd94a865704593cad116ae3fc48996
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 01:02:25 +0200

    Add loading & call hotels list from state

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <loading v-if="isLoading" />
+    <router-view v-else />
   </div>
 </template>
 
+<script>
+import { mapActions, mapState } from "vuex";
+import loading from "@/components/loading";
+
+export default {
+  name: "App",
+  components: {
+    loading
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.hotelsList.isLoading
+    })
+  },
+  methods: {
+    ...mapActions({
+      //Hotels List actions
+      loadHotelsList: "hotelsList/LOAD_HOTELS_LIST"
+    })
+  },
+  created() {
+    this.loadHotelsList();
+  }
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background: $bg-classroom;
 }
 </style>
