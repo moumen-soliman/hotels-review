00e908a90173dee0e572d9dc85e08d58d23c0ad4
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 01:02:51 +0200

    Create gallery
    Creating gallery and select image to display by using index of image

---
+++ src/components/gallery.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img :src="pictures[selectedIndex].photo" />
+    <div
+      v-for="(key, index) in pictures"
+      :key="index"
+      :class="{ active: index == selectedIndex }"
+    >
+      <img :src="key.thumbnail" @click="selectedIndex = index" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gallery",
+  props: {
+    pictures: Array
+  },
+  data() {
+    return {
+      // Add first image index as a default till user choose image
+      selectedIndex: 0
+    };
+  }
+};
+</script>
