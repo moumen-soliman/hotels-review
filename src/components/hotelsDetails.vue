d493e1e86e3240465e1b581e077682e31d621c84
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 01:12:40 +0200

    Create Hotels Details component
    Create Hotel Details component that recieve current selected id from function passing by hotel list component, and contain reviews & gallery components

---
+++ src/components/hotelsDetails.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-if="isLoading">
+      <loading />
+    </div>
+    <div v-else>
+      <reviews :reviews="reviews" />
+      <gallery :pictures="hotelDetailsImages" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import gallery from "./gallery";
+import reviews from "./reviews";
+import loading from "./loading";
+
+export default {
+  name: "hotelsDetails",
+  components: {
+    gallery,
+    reviews,
+    loading
+  },
+  computed: {
+    ...mapState({
+      // Hotel Details actions
+      hotelDetails: state => state.hotelDetails.all,
+      hotelDetailsImages: state => state.hotelDetails.all.pictures,
+      reviews: state => state.hotelDetails.all.reviews,
+      isLoading: state => state.hotelDetails.isLoading
+    })
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
