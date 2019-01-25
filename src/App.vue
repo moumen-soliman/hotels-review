aaefc744fcfe392a57f95e8e25ccc6e54202df2a
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 18:00:52 +0200

    Update parent component main style

--- src/App.vue
+++ src/App.vue
@@ -32,12 +32,15 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: $light-gray !important;
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: $black;
+    margin-top: 60px;
+  }
 }
 </style>
