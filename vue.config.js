d6f0e013b1e6127559ec73db3a923342fc9da36b
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 18:16:56 +0200

    Add purgeCSS and upload dist file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
-const path = require("path");
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const path = require('path');
 
 module.exports = {
+  configureWebpack: {
+    // Merged into the final Webpack config
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js')
+        ])
+      })
+    ]
+  },
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
