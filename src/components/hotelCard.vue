d34a7b839d6fb0b18493e733525f4e8cfb032f54
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 15:11:34 +0200

    Add price per night result

--- src/components/hotelCard.vue
+++ src/components/hotelCard.vue
@@ -9,12 +9,18 @@
       :key="key.id"
     >
       <a @click="getSelectedHotelDetails(key.id)">{{ key }}</a>
+      {{
+        currentSelectedID === key.id
+          ? night * key.pricePerNight + " per " + night
+          : key.pricePerNight + " per " + 1
+      }}
+      {{ getScoreOfHotel(key.totalScore) }}
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapState } from "vuex";
 
 export default {
   name: "hotelCard",
@@ -22,18 +28,43 @@ export default {
     hotelsList: Array,
     hotelsID: Array
   },
-  methods: {
-    ...mapActions({
-      // Hotel Details actions
-      getSelectedHotelDetails: "hotelDetails/LOAD_HOTEL_SELECTED_DETAILS_LIST"
-    })
-  },
   computed: {
+    ...mapState({
+      // Price states
+      night: state => state.price.night
+    }),
     // Add first hotel ID as selected ID till user choose one
     currentSelectedID() {
       let currentID = this.$store.state.hotelDetails.currentSelectedID;
       return currentID ? currentID : this.hotelsID;
     }
+  },
+  methods: {
+    ...mapActions({
+      // Hotel Details actions
+      getSelectedHotelDetails: "hotelDetails/LOAD_HOTEL_SELECTED_DETAILS_LIST"
+    }),
+    // Switch/case take total score and return the status of Hotel
+    getScoreOfHotel(totalScore) {
+      let message;
+      switch (true) {
+        case totalScore > 0 && totalScore <= 5:
+          message = 'Not recommended';
+          break;
+        case totalScore > 5 && totalScore <= 7:
+          message = 'good';
+          break;
+        case totalScore > 7 && totalScore <= 8:
+          message = 'Very good';
+          break;
+        case totalScore > 8 && totalScore <= 10:
+          message = 'Excellent';
+          break;
+        default:
+          message = 'No score yet';
+      }
+      return message;
+    },
   },
   created() {
     // Call get hotel details function and passing first index of hotelsID Array
