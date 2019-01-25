5c2e36e0443ea235fc0c1aa93c5247fd4d456bd9
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 18:32:07 +0200

    Update https on api link

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () =>
   axios.create({
-    baseURL: "http://my-json-server.typicode.com/fly365com/code-challenge",
+    baseURL: "https://my-json-server.typicode.com/fly365com/code-challenge",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
