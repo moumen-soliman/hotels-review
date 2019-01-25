4ac2519d847e486324330b35eb4e426bb05f3295
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-25 18:01:39 +0200

    Add gallery style

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <img :src="pictures[selectedIndex].photo" />
-    <div
-      v-for="(key, index) in pictures"
-      :key="index"
-      :class="{ active: index == selectedIndex }"
-    >
-      <img :src="key.thumbnail" @click="selectedIndex = index" />
+  <div class="gallery">
+    <div class="row">
+      <div class="col-12 gallery__main-image">
+        <img :src="pictures[selectedIndex].photo" />
+      </div>
+    </div>
+    <div class="wrap">
+      <div
+        v-for="(key, index) in pictures"
+        :key="index"
+        :class="{ active: index == selectedIndex }"
+      >
+        <div>
+          <img :src="key.thumbnail" @click="selectedIndex = index" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,3 +33,54 @@ export default {
   }
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.gallery {
+  &__main-image {
+    img {
+      width: 40%;
+    }
+  }
+  .wrap {
+    white-space: nowrap;
+    border: 0;
+    overflow-x: scroll;
+    max-width: 50%;
+    margin: 20px auto;
+    /* width */
+    &::-webkit-scrollbar {
+      height: 6pt;
+    }
+    /* Track */
+    &::-webkit-scrollbar-track {
+      background: #f1f1f1;
+    }
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+      background: #aaaab2;
+      border-radius: 50px;
+    }
+    /* Handle on hover */
+    &::-webkit-scrollbar-thumb:hover {
+      background: #aaaab2;
+    }
+    div {
+      display: inline-block;
+      margin-right: 20px;
+      margin-bottom: 5px;
+      img {
+        opacity: 0.5;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      &:hover,
+      &.active {
+        img {
+          opacity: 1;
+        }
+      }
+    }
+  }
+}
+</style>
