fbc2975cc2ef93f63042c9dc0d713848b187d1d7
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-26 19:37:48 +0200

    fix route format

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/Home.vue')
+      path: "/",
+      name: "home",
+      component: Home
     }
   ]
-})
+});
