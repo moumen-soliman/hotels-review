54655fd45e6f49327d320506bc69037606f67c70
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-26 18:45:23 +0200

    Fix format

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-const glob = require('glob-all');
-const path = require('path');
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
+const path = require("path");
 
 module.exports = {
   configureWebpack: {
@@ -8,9 +8,9 @@ module.exports = {
     plugins: [
       new PurgecssPlugin({
         paths: glob.sync([
-          path.join(__dirname, './src/index.html'),
-          path.join(__dirname, './**/*.vue'),
-          path.join(__dirname, './src/**/*.js')
+          path.join(__dirname, "./src/index.html"),
+          path.join(__dirname, "./**/*.vue"),
+          path.join(__dirname, "./src/**/*.js")
         ])
       })
     ]
